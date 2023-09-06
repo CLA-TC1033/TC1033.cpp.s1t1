1e0852f2e38385171251d9f70931446533ab6883
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2023-09-06 12:01:42 -0700

    Update exercise.cpp

--- exercise.cpp
+++ exercise.cpp
@@ -6,11 +6,6 @@
 
 int main()
 {
-//  operaNumeros();
-//  std::cout << "Suma de pares entre 0 y 100:" << sumaPares() << "\n";
-//  serieArmonica(1, 3, 25);
-//  serieArmonica(2,5,10);
-
-    std::cout << "Media geometrica de 3=" << mediaGeometrica(3) << "\n";
-    std::cout << "Media armonica de 3=" << mediaArmonica(3) << "\n";
+    
+    return 0;
 }
